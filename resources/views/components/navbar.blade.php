a43a695d788aae9e4e334e49d9e6fa5402b62eb4
SteveMhs01 <rasmaalba@gmail.com>
2024-08-11 21:41:09 +0700

    navbar bang

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,23 @@
 <body>
-    <div class="bg-white h-[67px] flex items-center justify-between px-4">
-        <div class="flex items-center">
+    <div class="bg-white h-[67px] flex items-center justify-between px-4 lg:justify-start">
+        <div class="flex items-center lg:hidden">
             <i id="menu-toggle" class="fa-solid fa-bars text-black text-xl cursor-pointer ml-[10px]"></i>
         </div>
-        <img src="{{ asset('assets/logo.jpg') }}" alt="" class="w-[148px] h-[26px] mx-auto">
+        <img src="{{ asset('assets/logo.jpg') }}" alt="" class="w-[148px] h-[26px] mx-auto mx-auto lg:mx-0 lg:mr-auto">
+        <ul class="flex space-x-4 mt-4 hidden lg:flex mb-4 mr-4">
+    <li><a href="#" class="py-2 px-4">Home</a></li>
+    <li class="relative">
+        <a href="#" class="py-2 px-4 hover:bg-gray-100" id="categories-toggle1">Categories</a>
+        <div class="absolute left-0 mt-2 w-48 bg-white border border-gray-200 dropdown-content" id="categories-dropdown1">
+            <a href="#" class="py-2 px-4 block hover:bg-gray-100">Mouse</a>
+            <a href="#" class="py-2 px-4 block hover:bg-gray-100">Keyboard</a>
+            <a href="#" class="py-2 px-4 block hover:bg-gray-100">Audio</a>
+            <a href="#" class="py-2 px-4 block hover:bg-gray-100">Gears</a>
+        </div>
+    </li>
+    <li><a href="#" class="py-2 px-4">Services</a></li>
+</ul>
+
         <i class="fa-regular fa-user bg-[#D9D9D9] text-[15px] p-[6px] mr-[10px] rounded-full"></i>
     </div>
     <div id="sidebar">
@@ -12,12 +26,14 @@
         </div> 
         <ul class="mt-4">
             <li><a href="#" class="py-2 px-4">Home</a></li>
-            <li><a href="#" class="py-2 px-4" id="categories-toggle">Categories</a></li>
+            <div class="box border-solid">
+            <li><a href="#" class="py-2 px-4 hover:bg-gray-100" id="categories-toggle">Categories</a></li>
             <div class="dropdown-content" id="categories-dropdown">
-                <a href="" class="py-2 px-4">Mouse</a>
-                <a href="" class="py-2 px-4">Keyboard</a>
-                <a href="" class="py-2 px-4">Audio</a>
-                <a href="" class="py-2 px-4">Gears</a>
+                <a href="" class="py-2 px-4 block">Mouse</a>
+                <a href="" class="py-2 px-4 block">Keyboard</a>
+                <a href="" class="py-2 px-4 block">Audio</a>
+                <a href="" class="py-2 px-4 block">Gears</a>
+            </div>
             </div>
             <li><a href="#" class="py-2 px-4">Services</a></li>
         </ul>
@@ -34,6 +50,16 @@
         document.getElementById('categories-toggle').addEventListener('click', function(event) {
             event.preventDefault(); // Prevent default link behavior
             const dropdown = document.getElementById('categories-dropdown');
+            if (dropdown.classList.contains('open')) {
+                dropdown.classList.remove('open');
+            } else {
+                dropdown.classList.add('open');
+            }
+        });
+
+        document.getElementById('categories-toggle1').addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent default link behavior
+            const dropdown = document.getElementById('categories-dropdown1');
             if (dropdown.classList.contains('open')) {
                 dropdown.classList.remove('open');
             } else {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
-<body  class="bg-[#D4D4D4]">
+
+<body class="bg-[#D4D4D4]">
     <x-Navbar />
-        
+
+    <div class="content">
+        <div class="relative mt-8 flex justify-center">
+            <input
+                type="text"
+                id="search"
+                placeholder=" "
+                class="peer w-full px-4 py-2 border rounded-lg shadow-sm focus:border-blue-500 focus:outline-none" />
+            <label for="search" class="absolute top-0 text-gray-500 transition-transform transform -translate-y-1 scale-75 origin-top-left peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-3 peer-placeholder-shown:text-gray-400 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-black-500">
+                <p class="text-[13px]">Search products</p>  
+            </label>
+            <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            <div class="cart bg-white flex">
+            <button type="submit"><i class="fa-solid fa-cart-shopping"></i></button>
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
