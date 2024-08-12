e950da46c0b4ee26260fcc3b56a139244b1d1149
YooSadewa <tugasthyy@gmail.com>
2024-08-12 12:50:35 +0700

    add loginform

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,23 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/stylelogin.css') }}">
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        * {
+            margin: 0;
+            right: 0;
+        }
+    </style>
 </head>
 <body class="">
-    <div class="background" style="background-image: linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url('/assets/bglogin.jpg');">
-        <div class="form-container w-[330px] h-[100vh] m-auto flex flex-column">
-            <div class="w-full bg-[#FFF] h-[405px] m-auto rounded-[15px]">
-                <img src="{{ asset('assets/logo.jpg') }}" alt="" class="mx-auto mt-[25px]">
-                <div class="w-[200px] h-[40px] bg-[#D9D9D9] font-bold mx-auto mt-[35px] rounded-full flex flex-row items-center">
-                    <button class="w-[97px] h-[40px] bg-[#E3001A] text-white text-center items-center flex justify-center ml-0 rounded-full">Log in</button>
+    <div class="background w-100% h-100vh bg-cover bg-no-repeat justify-center" style="background-image: linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url('/assets/bglogin.jpg');">
+        <div class="form-container w-[330px] md:w-[620px] lg:w-[730px] h-[100vh] m-auto flex flex-col">
+            <div class="bg-[#FFF] min-w-full h-[405px] md:h-[765px] lg:h-[565px] m-auto xl:ml-[200px] rounded-[15px] md:rounded-[24px]">
+                <img src="{{ asset('assets/logo.jpg') }}" alt="" class="mx-auto mt-[25px] md:mt-[47px] lg:mt-[17px] h-[26px] md:h-[49px] lg:h-[42px]">
+                <div class="w-[200px] md:w-[365px] lg:w-[315px] h-[40px] md:h-[70px] lg:h-[60px] bg-[#D9D9D9] font-bold mx-auto mt-[35px] md:mt-[65px] lg:mt-[20px] rounded-full flex flex-row items-center text-[15px] md:text-[24px]">
+                    <button class="w-[97px] md:w-[185px] lg:w-[160px] h-full bg-[#E3001A] text-white text-center items-center flex justify-center ml-0 rounded-full">Log in</button>
                     <button class="flex-grow text-center">Sign Up</button>
                 </div>
-                <form action="" class="flex flex-col justify-center mt-[30px]">
-                    <input type="text" placeholder="Username" class="border-b-[2px] border-black w-[280px] pb-[5px] mx-auto mb-[55px]">
-                    <input type="text" placeholder="Password" class="border-b-[2px] border-black w-[280px] pb-[5px] mx-auto">
-                    <a href="#" class="text-end mr-[25px] mt-[15px]">Forgot My Password?</a>
-                    <input type="submit" value="Submit" class="border-black border-[1px] w-[175px] mx-auto rounded-[15px] mt-[30px] h-[40px]">
+                <form action="" class="flex flex-col justify-center mt-[30px] md:mt-[105px] lg:mt-[75px]">
+                    <input type="text" placeholder="Username" class="border-b-[2px] border-black w-[280px] md:w-[525px] lg:w-[600px] pb-[5px] mx-auto mb-[55px] md:mb-[105px] lg:mb-[72px] md:text-[20px]">
+                    <input type="text" placeholder="Password" class="border-b-[2px] border-black w-[280px] md:w-[525px] lg:w-[600px] pb-[5px] mx-auto md:text-[20px]">
+                    <a href="#" class="text-end mr-[25px] md:mr-[50px] lg:mr-[65px] mt-[15px] md:text-[24px] lg:text-[20px]">Forgot My Password?</a>
+                    <input type="submit" value="Submit" class="border-black border-[1px] w-[175px] md:w-[325px] lg:w-[250px] lg: mx-auto rounded-[15px] md:rounded-[24px] mt-[30px] md:mt-[70px] h-[40px] md:h-[75px] lg:h-[55px] md:text-[24px] lg:text-[18px]">
                 </form>
             </div>
         </div>
