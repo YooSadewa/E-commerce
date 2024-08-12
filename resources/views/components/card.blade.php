c19ed67190123b5c8cfd60cfd1ca9d9852be3773
SteveMhs01 <rasmaalba@gmail.com>
2024-08-12 13:37:29 +0700

    add card

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,72 @@
+<div class="card-group w-full flex justify-center mt-4">
+          <div class="flex flex-col">
+          <div class="card w-[220px] h-[95px] border-[1px] rounded-[12px] border-black bg-white sm:w-[350px] sm:h-[125px]">
+                <div class="card-img mt-[2.5px] ml-[2.5px] sm:mt-[3px]">
+                    <img class="w-[106px] rounded-[15px] h-[88px] object-cover sm:w-[150px] sm:h-[118px]" src="{{asset('assets/alia.jpg')}}" alt="">
+                </div>
+                <div class="card-header relative w-[88px] alia sm:w-[150px] sm:left-[50%]">
+                    <p class="text-[8px] sm:text-[9px]">Fantech X MAHA5 ALIA ADELIA</p>
+                    <div class="flex justify-between sm:mt-1">
+                        <p class="text-[red] text-[7px] font-medium">In Stock</p>
+                        <div class="kotak w-[22px] h-[10px] bg-[#D9D9D9] rounded-[3px] flex items-center justify-center"><i class="fa-solid fa-star text-[6px] text-[#EAB700]"></i>
+                            <p class="text-[6px]">4,9</p>
+                        </div>
+                    </div>
+                    <p class="text-[10px] sm:text-[11px]">Rp. 299.000</p>
+                </div>
+                <div class="card-footer relative alia mt-1.5 w-[90px] sm:w-[152px] sm:mt-9 sm:left-[50%]">
+                    <p class="text-[5px] sm:text-[6px] font-sans flex justify-end mr-0.5">10rb+ Terjual</p>
+                    <div class="beli w-[90px] h-[19px] bg-[#FF0000] rounded-[5px] flex items-center justify-center font-sans sm:w-[152px]">
+                        <p class="text-[8px] text-[white] font-semibold ">Masukkan Ke Troli</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card w-[220px] h-[95px] border-[1px] rounded-[12px] border-black bg-white mt-1.5 sm:w-[350px] sm:h-[125px]  ">
+                <div class="card-img mt-[2.5px] ml-[2.5px] sm:mt-[3px]">
+                    <img class="w-[106px] rounded-[15px] h-[88px] object-cover sm:w-[150px] sm:h-[118px]" src="{{asset('assets/alia.jpg')}}" alt="">
+                </div>
+                <div class="card-header relative w-[88px] alia sm:w-[152px] sm:left-[50%]">
+                    <p class="text-[8px] sm:text-[9px]">Fantech X MAHA5 ANDI ADINATA</p>
+                    <div class="flex justify-between sm:mt-1">
+                        <p class="text-[red] text-[7px] font-medium">In Stock</p>
+                        <div class="kotak w-[22px] h-[10px] bg-[#D9D9D9] rounded-[3px] flex items-center justify-center"><i class="fa-solid fa-star text-[6px] text-[#EAB700]"></i>
+                            <p class="text-[6px]">4,9</p>
+                        </div>
+                    </div>
+                    <p class="text-[10px] sm:text-[11px]">Rp. 299.000</p>
+                </div>
+                <div class="card-footer relative alia mt-1.5 w-[90px] sm:w-[152px]  sm:mt-9 sm:left-[50%]">
+                    <p class="text-[5px] sm:text-[6px] font-sans flex justify-end mr-0.5">10rb+ Terjual</p>
+                    <div class="beli w-[90px] h-[19px] bg-[#FF0000] rounded-[5px] flex items-center justify-center font-sans sm:w-[152px]">
+                        <p class="text-[8px] text-[white] font-semibold ">Masukkan Ke Troli</p>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+            <div class="card w-[100px] h-[195px] border-[1px] rounded-[12px] border-black bg-white ml-2 sm:w-[150px] sm:h-[255px]">
+                <div class="card-img mt-[2.5px] flex justify-center">
+                    <img class="w-[90px] rounded-[15px] h-[85px] object-cover sm:w-[145px] sm:h-[100px]" src="{{asset('assets/all.jpg')}}" alt="">
+                </div>
+                <div class="card-header w-[88px] ml-1.5 sm:mt-4 sm:w-[140px]">
+                    <p class="text-[8px] sm:text-[9px]">Fantech X MAHA5 ALL TALENT</p>
+                    <div class="flex justify-between sm:mt-1">
+                        <p class="text-[red] text-[7px] font-medium">In Stock</p>
+                        <div class="kotak w-[22px] h-[10px] bg-[#D9D9D9] rounded-[3px] flex items-center justify-center"><i class="fa-solid fa-star text-[6px] text-[#EAB700]"></i>
+                            <p class="text-[6px]">4,9</p>
+                        </div>
+                    </div>
+                    <p class="text-[10px] sm:text-[11px]">Rp. 299.000</p>
+                </div>
+                <div class="card-footer ml-1 mt-6 w-[90px] sm:w-[140px] sm:mt-14">
+                    <p class="text-[5px] sm:text-[6px] font-sans flex justify-end mr-0.5 sm:text-[6px]">10rb+ Terjual</p>
+                    <div class="beli w-[90px] h-[19px] bg-[#FF0000] rounded-[5px] flex items-center justify-center font-sans sm:w-[140px] sm:h-[]">
+                        <p class="text-[8px] text-[white] font-semibold ">Masukkan Ke Troli</p>
+                    </div>
+                </div>
+            </div>
+
+            
+
+        </div>
+    </div>
