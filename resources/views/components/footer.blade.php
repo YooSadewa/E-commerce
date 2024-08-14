ba66a6dd20cb1aa32da28a0c2ada5ddb1248a8c5
SteveMhs01 <rasmaalba@gmail.com>
2024-08-14 12:07:16 +0700

    add card

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,29 +1,29 @@
-<div class="footer w-full bg-white h-full mt-4 rounded-t-lg">
+<div class="footer w-full bg-white h-full mt-4 rounded-t-lg lg:mt-8">
         <div class="ml-4 lg:flex lg:ml-8 md:ml-8">
             <div class="box1">
         <div class="footer-img relative top-3">
             <img src="{{asset('assets/logo.jpg')}}" alt="">
         </div>
-        <p class="mt-5 text-[16px]  font-light">Question?</p>
-        <p class="text-[19px]  font-bold">0812345678</p>
-        <p class="mt-2 text-[16px]  font-light">Follow Us</p>
+        <p class="mt-5 text-[12px]  font-light md:text-[14px] lg:text-[16px]">Question?</p>
+        <p class="text-[15px]  font-bold md:text-[19px]">0812345678</p>
+        <p class="mt-2 text-[12px]  font-light md:text-[14px] lg:text-[16px]">Follow Us</p>
         <div class="flex mt-2">
        <a href="#"> <i class="fa-brands fa-facebook text-[25px] text-[blue]"></i> </a>
        <a href="#"> <i class="fa-brands fa-x-twitter text-[25px] ml-2"></i> </a>
         <a href="#"> <i class="fa-brands fa-youtube text-[25px] text-[red] ml-2"></i> </a>
         <a href="#"> <i class="fa-brands fa-instagram text-[25px] ml-2"></i> </a>
      </div>
-     <p class="text-[18px] font-bold mt-2">Our Store</p>
-     <p class="mt-2 text-[16px]  font-light w-[300px] md:w-[700px] lg:w-[470px]  sm:w-[550px] xl:w-[580px]">Fantech Indonesia Sedayu Square Blok J No. 27 Jl. Lingkar Luar Barat Cengkareng - Jakarta Barat 11730</p>
+     <p class="text-[14px] font-bold mt-2 md:text-[14px] lg:text-[16px]">Our Store</p>
+     <p class="mt-2 text-[12px] md:text-[14px] lg:text-[16px]  font-light w-[300px] md:w-[700px] lg:w-[470px]  sm:w-[550px] xl:w-[580px]">Fantech Indonesia Sedayu Square Blok J No. 27 Jl. Lingkar Luar Barat Cengkareng - Jakarta Barat 11730</p>
      </div>
      <div class="box-2 lg:ml-12 xl:ml-20">
-     <p class="text-[18px] font-bold mt-2">Dealer Welcome</p>
-     <p class="mt-2 text-[16px]  font-light w-[300px] md:w-[700px] lg:w-[470px] sm:w-[550px] xl:w-[580px]">CV. GOLDEN TECHNOLOGY INDO
+     <p class="text-[14px] font-bold mt-2 md:text-[14px] lg:text-[16px]">Dealer Welcome</p>
+     <p class="mt-2 text-[12px] md:text-[14px] lg:text-[16px]  font-light w-[300px] md:w-[700px] lg:w-[470px] sm:w-[550px] xl:w-[580px]">CV. GOLDEN TECHNOLOGY INDO
 Sedayu Square Blok J no. 26 Jl. Lingkar Luar Barat Cengkareng
 Jakarta Barat 11730
 sales@goldentech.id marketingfantech@gmail.com
 Text for more in Sales 1: +62 852-8333-3267 Sales 2: +62 811-1903-2720</p>
-<p class="mt-6 text-[16px]  font-light w-full mb-4 lg:mt-[70px]">Copyright © 2023 Fantech. All Rights Reserved</p>
+<p class="mt-6 text-[12px] md:text-[14px] lg:text-[16px]  font-light w-full mb-4 lg:mt-[70px]">Copyright © 2023 Fantech. All Rights Reserved</p>
 </div>
         </div>
         <div class="w-full h-[15px] bg-[red]"></div>
