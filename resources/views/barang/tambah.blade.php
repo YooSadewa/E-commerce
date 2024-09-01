cb54e06f69532075c890d3cd3eca84196080b071
YooSadewa <tugasthyy@gmail.com>
2024-09-01 23:19:08 +0700

    edit style tambah barang

--- resources/views/barang/tambah.blade.php
+++ resources/views/barang/tambah.blade.php
@@ -22,62 +22,80 @@
     <div class="w-full h-full my-[63px] mt-[20px] flex">
         <form action="" class="relative w-[345px] flex flex-col m-auto h-[645px] bg-[#FFF] px-[32px] pt-[45px] gap-[40px] rounded-[12px]
         md:w-[700px] md:h-[1000px] md:px-[50px] md:pt-[90px] md:gap-[80px]
-        lg:w-[1200px] lg:h-[570px] lg:flex-row lg:justify-between lg:gap-[60px]" 
+        lg:w-[1200px] lg:h-[570px] lg:flex lg:flex-col lg:items-center lg:gap-[40px]" 
         method="POST" enctype="multipart/form-data">
-        <h1 class="text-center font-bold -my-[15px] text-[#000]">Tambahkan Barang</h1>
             
-            <!-- Container for Nama, Harga, Stok -->
-            <div class="flex flex-col gap-[35px] md:gap-[80px] lg:gap-[90px] lg:w-1/2">
-                <div class="relative">
-                    <input type="text" id="nama_barang" class="peer border-b-[1px] border-black w-full placeholder-transparent focus:outline-none focus:border-blue-500 mt-[5px] md:text-[24px]" placeholder=" " />
-                    <label for="nama_barang" class="absolute text-base text-[#000] duration-300 transform -translate-y-6 scale-75 top-1 left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:-translate-x-3
+            <h1 class="text-center font-bold -my-[25px] text-[#000] md:text-[36px] lg:text-[36px] lg:-my-[45px] lg:mb-0">Tambahkan Barang</h1>
+            
+            <div class="flex flex-col lg:flex-row lg:w-full lg:gap-[60px]">
+                <!-- Container for Nama, Harga, Stok -->
+                <div class="flex flex-col gap-[35px] md:gap-[60px] lg:w-1/2">
+                    <div class="relative">
+                        <input type="text" id="nama_barang" class="peer border-b-[1px] border-black w-full placeholder-transparent focus:outline-none focus:border-blue-500 mt-[5px] md:text-[24px]" placeholder=" " />
+                        <label for="nama_barang" class="absolute text-base text-[#000] duration-300 transform -translate-y-6 scale-75 top-1 left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:-translate-x-3
                                                     md:text-[24px] md:peer-focus:-translate-x-5">
-                        Nama Barang
-                    </label>
-                </div>
-            
-                <div class="relative">
-                    <input type="text" id="harga_barang" class="peer border-b-[1px] border-black w-full placeholder-transparent focus:outline-none focus:border-blue-500 mt-[5px] md:text-[24px]" placeholder=" " />
-                    <label for="harga_barang" class="absolute text-base text-[#000] duration-300 transform -translate-y-6 scale-75 top-1 left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:-translate-x-3
-                                                        md:text-[24px] md:peer-focus:-translate-x-5">
-                        Harga Barang
-                    </label>
-                </div>
-            
-                <div class="relative">
-                    <input type="text" id="stok_barang" class="peer border-b-[1px] border-black w-full placeholder-transparent focus:outline-none focus:border-blue-500 mt-[5px] md:text-[24px]" placeholder=" " />
-                    <label for="stok_barang" class="absolute text-base text-[#000] duration-300 transform -translate-y-6 scale-75 top-1 left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:-translate-x-3
-                                                        md:text-[24px] md:peer-focus:-translate-x-5">
-                        Stok Barang
-                    </label>
-                </div>
-            
-                <!-- Container for Foto Barang -->
-                <div class="relative">
-                    <label class="block mb-2 text-base text-[#000]">Foto Barang</label>
-                    <div class="w-full h-[25px] text-[12px] rounded-[12px] border-black border-[1px] text-center flex items-center justify-center text-[#000] cursor-pointer" onclick="document.getElementById('foto_barang').click()">
-                        File Chosen
+                            Nama Barang
+                        </label>
                     </div>
-                    <input type="file" id="foto_barang" class="hidden" onchange="document.getElementById('file-name').textContent = this.files[0].name;" />
-                    <div id="file-name" class="mt-2 text-[#000]"></div>
+                    
+                    <div class="relative">
+                        <input type="text" id="harga_barang" class="peer border-b-[1px] border-black w-full placeholder-transparent focus:outline-none focus:border-blue-500 mt-[5px] md:text-[24px]" placeholder=" " />
+                        <label for="harga_barang" class="absolute text-base text-[#000] duration-300 transform -translate-y-6 scale-75 top-1 left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:-translate-x-3
+                                                        md:text-[24px] md:peer-focus:-translate-x-5">
+                            Harga Barang
+                        </label>
+                    </div>
+                    
+                    <div class="relative">
+                        <input type="text" id="stok_barang" class="peer border-b-[1px] border-black w-full placeholder-transparent focus:outline-none focus:border-blue-500 mt-[5px] md:text-[24px]" placeholder=" " />
+                        <label for="stok_barang" class="absolute text-base text-[#000] duration-300 transform -translate-y-6 scale-75 top-1 left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:-translate-x-3
+                                                        md:text-[24px] md:peer-focus:-translate-x-5">
+                            Stok Barang
+                        </label>
+                    </div>
+                </div>
+    
+                <!-- Container for Foto, Deskripsi, and Kategori -->
+                <div class="flex flex-col lg:w-1/2 lg:ml-[20px]">
+                    <!-- Foto Barang -->
+                    <div class="relative mb-[35px]">
+                        <label class="block mb-2 text-base text-[#000] md:text-[24px]">Foto Barang</label>
+                        <div class="w-full h-[25px] md:h-[35px] md:rounded-[18px] text-[12px] md:text-[18px] rounded-[12px] border-black border-[1px] text-center flex items-center justify-center text-[#000] cursor-pointer" onclick="document.getElementById('foto_barang').click()">
+                            File Chosen
+                        </div>
+                        <input type="file" id="foto_barang" class="hidden" onchange="document.getElementById('file-name').textContent = this.files[0].name;" />
+                        <div id="file-name" class="mt-2 text-[#000]"></div>
+                    </div>
+    
+                    <!-- Deskripsi Barang -->
+                    <div class="relative">
+                        <textarea id="deskripsi_barang" cols="30" rows="10" class="peer border-b-[1px] border-black w-full h-[65px] placeholder-transparent focus:outline-none focus:border-blue-500 -mt-[15px] md:text-[24px] md:h-[120px] lg:max-h-[300px] lg:min-h-[37px] lg:h-[37px] lg:" placeholder=" "></textarea>
+                        <label for="deskripsi_barang" class="absolute text-base text-[#000] duration-300 transform -translate-y-6 scale-75 top-1 left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:-translate-x-4 -mt-[20px]
+                                                                md:text-[24px] md:peer-focus:-translate-x-5
+                                                                lg:mt-0">
+                            Deskripsi Barang
+                        </label>
+                    </div>
+    
+                    <!-- Kategori Barang -->
+                    <div class="relative">
+                        <select id="kategori_barang" name="kategori_barang" class="border-b-[1px] border-black w-full focus:outline-none focus:border-blue-500 mt-[5px] appearance-none md:text-[24px]">
+                            <option value="" disabled selected hidden>Kategori Barang</option>
+                            <option value="mouse">Mouse</option>
+                            <option value="keyboard">Keyboard</option>
+                            <option value="audio">Audio</option>
+                            <option value="gears">Gears</option>
+                        </select>
+                    </div>
                 </div>
             </div>
-            
-            <!-- Container for Deskripsi -->
-            <div class="relative lg:w-1/2">
-                <textarea id="deskripsi_barang" cols="30" rows="10" class="peer border-b-[1px] border-black w-full h-[220px] placeholder-transparent focus:outline-none focus:border-blue-500 mt-[5px] md:text-[24px] md:h-[300px] lg:max-h-[300px] lg:min-h-[37px] lg:h-[37px]" placeholder=" "></textarea>
-                <label for="deskripsi_barang" class="absolute text-base text-[#000] duration-300 transform -translate-y-6 scale-75 top-1 left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:-translate-x-4
-                                                             md:text-[24px] md:peer-focus:-translate-x-5">
-                    Deskripsi Barang
-                </label>
-            </div>
-            
-            
+    
             <!-- Submit Button -->
             <input type="submit" value="SUBMIT" name="submit" class="px-[60px] py-[10px] bg-[#FFF] hover:bg-[#E3001A] border-[2px] duration-[0.2s] border-[#E3001A] w-fit mx-auto mt-[30px] rounded-[15px] text-[#E3001A] hover:text-[#FFF] font-bold
-                                                                     md:px-[120px] md:py-[20px] md:rounded-[30px] md:text-[24px] md:mt-[10px] lg:absolute lg:bottom-[30px] lg:left-1/2 lg:transform lg:-translate-x-1/2">
+                                                                md:px-[120px] md:py-[20px] md:rounded-[30px] md:text-[24px] md:-mt-[30px] lg:w-[300px] lg:mt-[40px]">
         </form>
     </div>
+    
     
     <div class="w-full h-[15px] bg-[#E3001A] -mt-[100px]"></div>
 </body>
