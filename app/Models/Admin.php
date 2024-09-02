3398e04218bc67ae008f6ed228150785c3c1168f
YooSadewa <tugasthyy@gmail.com>
2024-09-02 14:06:36 +0700

    add tb_admin

---
+++ app/Models/Admin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tb_admin';
+
+    // Menggunakan primary key 'id_barang'
+    protected $primaryKey = 'id_admin';
+
+    // Primary key auto-increment
+    public $incrementing = true;
+
+    // Tipe data primary key
+    protected $keyType = 'int';
+
+    // Kolom yang boleh diisi secara massal
+    protected $fillable = [
+        'nama_admin',
+        'username',
+        'password',
+        'telp',
+        'email',
+        'foto_admin',
+    ];
+
+    // Timestamps
+    public $timestamps = true;
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/admin', function () {
+    return view('welcomeadmin');
 });
 
 Route::get('/login', function () {
@@ -15,7 +20,16 @@ Route::get('/signup', function () {
     return view('signup');
 })->name('signup');
 
+Route::get('/admin/profile', function () {
+    return view('admin/profile');
+})->name('profile');
+
 Route::resource('barang', BarangController::class);
 
 // Mengarahkan /admin/infoproduct ke BarangController@index
 Route::get('/admin/infoproduct', [BarangController::class, 'index'])->name('infoproduct');
+
+Route::resource('admin', AdminController::class);
+
+// Mengarahkan /admin/infoproduct ke BarangController@index
+Route::get('/admin/profile', [AdminController::class, 'index'])->name('profile');
